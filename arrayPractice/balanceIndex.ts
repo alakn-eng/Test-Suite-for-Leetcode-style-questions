fd12ccdfe68fdcd8c056f022d87759c10ffdfe49
alakn-eng <narayanan.aartil@gmail.com>
2025-10-21 11:28:37 -0400

    improved the solution to go from O-n to O-n^2

--- arrayPractice/balanceIndex.ts
+++ arrayPractice/balanceIndex.ts
@@ -3,28 +3,30 @@
 // If no such index exists, return -1.
 // If multiple exist, return the leftmost one.
 
-//helper fn to continually add a partial sum
-function sumOfPartialArray(arr: number[], startIndex: number): number {
-    let partialSum = 0;
+//helper fn to continually add a partial sum- removing to turn the soln into an O(n) vs O(n^2) when used
+// function sumOfPartialArray(arr: number[], startIndex: number): number {
+//     let partialSum = 0;
 
-    for (let i = startIndex; i < arr.length; i++) {
-        partialSum += arr[i]
-    }
+//     for (let i = startIndex; i < arr.length; i++) {
+//         partialSum += arr[i]
+//     }
 
-    return partialSum;
-}
+//     return partialSum;
+// }
 
 function balanceIndex(arr: number[]) {
     let leftSum = 0;
     let rightSum = 0;
+    let total = 0;
+    for (const x of arr) total += x;
 
     for (let i = 0; i < arr.length; i++) {
-        i == 0 ? null : leftSum += arr[i - 1] // continually update the leftSum
+        i === 0 ? null : leftSum += arr[i - 1] // continually update the leftSum
 
-        i + 1 < arr.length ? rightSum = sumOfPartialArray(arr, i + 1) : null //continually calcualate the rightSum
+        i + 1 < arr.length ? rightSum = total - leftSum - arr[i] : rightSum = 0 //continually calcualate the rightSum, question logic dictates rightSum needs to be 0 when the last element is hit
         console.log("leftSum:", leftSum)
         console.log("rightSum:", rightSum)
-        if (leftSum == rightSum) { //compare the value
+        if (leftSum === rightSum) { //compare the value
             console.log(i);
             return i;
         }
